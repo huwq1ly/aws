dc7a284259e4874aacbc71483eb24179f3ef33a8
huwq <huwq1ly@gmail.com>
2021-04-14 09:33:55 +0000

    edit CFn template

---
+++ CFn/template/00-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# deployオプションをつけないとチェンジセットの作成のみが実行される
+CHANGESET_OPTION="--no-execute-changeset"
+
+if [ $# = 1 ] && [ $1 = "deploy" ]; then
+  echo "deploy mode"
+  CHANGESET_OPTION=""
+fi
+
+# テンプレート名とスタック名
+CFN_TEMPLATE=''
+CFN_STACK_NAME=''
+
+# テンプレートの実行
+aws cloudformation deploy --stack-name ${CFN_STACK_NAME} --template-file ${CFN_TEMPLATE} ${CHANGESET_OPTION} 
